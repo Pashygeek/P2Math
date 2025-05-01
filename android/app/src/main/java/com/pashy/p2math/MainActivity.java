6676c9cd523fa9096453bfc8ef8fa31b94a4d95a
patiencewanjiru <patiencewanjiru123@gmail.com>
2025-05-01 12:42:08 +0300

    Made the calc website an apk

---
+++ android/app/src/main/java/com/pashy/p2math/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pashy.p2math;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pashy.p2math',
+  appName: 'P2Math',
+  webDir: 'build'
+};
+
+export default config;
